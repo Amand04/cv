a1481547e52dc10c385a79dfc24d2261b4f7a59c
Amandine <amandinejeanjules@free.fr>
2023-02-28 14:07:37 +0100

    update delete preventdefault

--- src/components/button/ButtonBeer.js
+++ src/components/button/ButtonBeer.js
@@ -5,7 +5,7 @@ import beer from "../../images/accueil_beer2.jpg";
 
 const Beer = () => {
   return (
-    <Link to={"/Beer"} className="Link" reloadDocument>
+    <Link to={"/Beer"} className="Link">
       <Card className="button">
         <Card.Img variant="top" src={beer} />
         <Card.Body>
